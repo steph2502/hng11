96766d9f162dc0179e6a6bd7d9fbfdb23582f8c6
Onwuagbaizu Stephanie <127240541+steph2502@users.noreply.github.com>
2024-07-06 07:47:21 +0100

    Update settings.py

--- Geogreet/settings.py
+++ Geogreet/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('django-insecure-9oa38wt^gin75(d^nt)zga)@y3%j_ncoj0_)s(4t=(q-ehzi(3')
+SECRET_KEY = 'django-insecure-9oa38wt^gin75(d^nt)zga)@y3%j_ncoj0_)s(4t=(q-ehzi(3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG','True') == 'True'
+DEBUG = 'True'
 # import os
 
 
- ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '.vercel.app,127.0.0.1').split(',')
+ ALLOWED_HOSTS = ['.vercel.app,127.0.0.1']
                 
 
 
